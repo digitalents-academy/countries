787861d7d0b5c4a958a7218c20a8598dec26184f
David Armeni <david.armeni1993@gmail.com>
2022-09-27 15:08:48 +0300

    filter progresses

--- resources/app.js
+++ resources/app.js
@@ -1,9 +1,11 @@
+// fetch all the info about the countries
 let response = fetch("https://restcountries.com/v3.1/all")
   .then((res) => res.json())
   .then((data) => {
     return data;
   });
 
+//create div with text fot all the countries
 async function getAll() {
   let data = await response;
   data.forEach((element) => {
@@ -15,7 +17,8 @@ async function getAll() {
     let capital = document.createElement("p");
 
     card.setAttribute("id", element.name.common.toLowerCase());
-    card.setAttribute("class", "card-div");
+    // card.setAttribute("class", "card-div");
+    card.classList.add("card-div", element.region.toLowerCase());
 
     name.textContent = element.name.common;
     flag.src = element.flags.png;
@@ -30,13 +33,52 @@ async function getAll() {
 
 getAll();
 
+// code for the search bar
+
+let searchBar = document.getElementById("search-bar");
+
+const cards = document.getElementsByClassName("card-div");
+const arr = Array.from(cards);
+console.log(arr);
+let selectedValue = document
+  .getElementById("filter-select")
+  .value.toLowerCase();
+
+searchBar.addEventListener("input", () => {
+  for (let i = 0; i < cards.length; i++) {
+    if (cards[i].id.includes(searchBar.value)) {
+      cards[i].style.display = "block";
+    } else {
+      cards[i].style.display = "none";
+    }
+  }
+});
+
 async function filter() {
-  let data = await response;
-  let selectedValue = document.getElementById("filter-select").value;
-  console.log(
-    data.filter((region) => region.region.toLowerCase() == selectedValue)
-  );
+  for (let i = 0; i < cards.length; i++) {
+    if (cards[i].classList.contains("europe")) {
+      cards[i].style.display = "block";
+    } else {
+      cards[i].style.display = "none";
+    }
+  }
 }
+
+// code for the select filter bar
+
+// async function filter() {
+//   let data = await response;
+//   let selectedValue = document.getElementById("filter-select").value;
+//   console.log(
+//     data.filter((region) => region.region.toLowerCase() == selectedValue)
+//   );
+//   if (selectedValue === cards.classList.contains("europe")) {
+//     console.log("asd");
+//     cards.style.display = "block";
+//   } else {
+//     cards.style.display = "none";
+//   }
+// }
 
 // let filter = fetch("https://restcountries.com/v3.1/all")
 // .then((res) => res.json())
